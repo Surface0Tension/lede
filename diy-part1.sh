9aa1df0a4b76604fd8aaf03bfcccbc91c32eec58
lzhang <52021580+Surface0Tension@users.noreply.github.com>
2021-07-13 01:59:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,7 +8,9 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+
+# N1 install program
+git clone https://github.com/tuanqing/install-program package/install-program
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
